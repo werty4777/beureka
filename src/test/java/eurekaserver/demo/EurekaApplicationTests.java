badf26b96fda74f6773da80c16740726ca94b2b2
Adrian Galindo <asdrian4455@hotmail.com>
2022-02-11 22:41:41 -0500

    first commit

---
+++ src/test/java/eurekaserver/demo/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package eurekaserver.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
